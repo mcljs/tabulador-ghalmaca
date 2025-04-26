c417185c0a8abb4a64fcf209d693b7c4cd1da625
Michael Chacon <mcljs15@gmail.com>
2025-04-26 14:12:16 -0400

    Add

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -726,7 +726,6 @@ export default function Hero() {
                       name="peso"
                       type="number"
                       min="1"
-                      value={peso}
                       onChange={(e) => setPeso(e.target.value)}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
                     />
